20458cd79125d13b66a48dca5ddd2f0c82e0cb0a
NareshBMV <inareshbabumv@gmail.com>
2018-03-24 12:42:51 +0530

    Completed Closure

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,151 +1,183 @@
-//: Playground - noun: a place where people can play
-//Without closure and with simple function as argument to closure
-let array = ["Bharath","Sunil","Naresh","Indira","Suresh"]
-func backward(_ s1:String,_ s2:String)->Bool {
+//Closures
+//Closures are self contained blocks that can be passed around and used in code
+
+//Closure Types
+//Global function - named and do not capture value
+//Nested function - named and capture value from enclosing function
+//Closure expression - non-named and capture values from surrounding context
+
+//Closure expression optimization
+//--Infering parameter types and return types from context
+//--Implicit return from single expression closure
+//--Shorhand arguement
+//--Trailing closure syntax
+
+//Closure Expression - Inline Closure
+//Sorted Method Sample
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+func backward(_ s1:String, _ s2:String) -> Bool {
+    return s1<s2
+}
+let values = names.sorted(by: backward)
+print(values)
+
+let values2 = names.sorted { (s1, s2) -> Bool in
     return s1>s2
 }
-let sortedWithoutClosure = array.sorted(by: backward)
-print(sortedWithoutClosure)
+print(values2)
 
-//Closure Expression syntax
-let expressionSyntax = array.sorted(by: {(s1:String, s2:String) -> Bool in return s1<s2 })
-print(expressionSyntax)
+//Closure Expression Syntax
+//in-out parameter can be used
+//variadic parameter can be used
+//But cannot have default values
 
-//infer type from context
-let typeFromContext = array.sorted(by: {
-    s1,s2 in return s1>s2
-})
+//Inferring type from context
+let withoutTypes = names.sorted(by: {s1,s2 in return s1>s2})
+print(withoutTypes)
 
-print(typeFromContext)
+//Implicit return from single expression closure
+let singleExpression  = names.sorted(by: {s1,s2 in s1>s2}) //Having single expression (s1>s2) so no need of return keyword
 
-//Single expression closure no need return keyword
-let singleExpression = array.sorted(by: {s1,s2 in s1>s2})
-print(singleExpression)
-
-//Shorthand argument names
-let shorthandArgs = array.sorted(by: {$0>$1})
-print(shorthandArgs)
+//Shorthand Argument names
+let shorthandArgs = names.sorted(by: {$0>$1})
+print("Shorthand : \(shorthandArgs)")
 
 //Operator Method
-let operatorMethod = array.sorted(by: <)
-print(operatorMethod)
+let operatorMethod = names.sorted(by: >)
+print("Operator Method : \(operatorMethod)")
 
-//Checking Trailing closure topic
-func checkClosure(closure:()->Void){
-    //Closure parameter
-    //Cannot have argument label
+//Trailing Closure
+//If a closure is passed as argument to function then we can write closure expression after function call parantheses
+//There is no need to write argument lable to a function call having closure
+func trailingClosure(closure:()->Void) {
+}
+//Normal Closure Call
+trailingClosure(closure: {
+})
+//Trailing Closure Call
+trailingClosure {
 }
 
-//Calling closure
-checkClosure(closure: {})
+//Sorting method with Trailing Closure
+let sortingTrailingClosure =  names.sorted(){$0>$1}
+print("sortingTrailingClosure : \(sortingTrailingClosure)")
 
-//Trailing closure
-checkClosure() {
-    
-}
+//If closure is the only argument to a function then calling function no need to have parentheses
+let noParenthesesClosure = names.sorted{$0>$1}
+print("noParenthesesClosure : \(noParenthesesClosure)")
 
-//Checking with sorted function call
-let arrayChecking = array.sorted(){$0>$1}
-print("Array Checking : \(arrayChecking)")
+//Demo of Array Method map to show trailing closure
+let digitsCount = [0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"]
+let number =  [12 ,555, 12345]
 
-//If closure is the only argument in the function then no need of "()"
-let arrayChecking2 = arrayChecking.sorted{$0>$1}
-print("Array Checking 2 : \(arrayChecking2)")
-
-let digitNames = [0:"Zero", 1:"One", 2:"Two",3:"Three", 4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine"]
-let numbers = [16,58,510]
-let stringArray = numbers.map{(number)->String in
+print("Mapping")
+let numberToString = number.map { (number) -> String in
+    var actualString = ""
     var num = number
-    var str = ""
     repeat {
-        str = digitNames[num%10]!+str
-        num /= 10
+        actualString = digitsCount[num%10]! + actualString
+        num = num/10
     }while num > 0
-    return str
+    return actualString
 }
+print("numberToString : \(numberToString)")
 
-print(stringArray)
-
-//Capturing values (Storing and accessing values in surrounding context)... Example illustrates nested function
-func makeIncrementer(forValue amount:Int)->()->Int {
-    var referenceAmount = 0
+//Capturing Values
+//Simplest form of capturing values is nested function
+//Nested function can capture values from its outer function
+func makeIncrement(Increment amount:Int)->()->Int {
+    var resultUpdate = 0
     func incrementer()->Int {
-        referenceAmount += amount
-        return referenceAmount
+        resultUpdate += amount
+        return resultUpdate
     }
     return incrementer
 }
 
-var valueChange = makeIncrementer(forValue: 10)
-print(valueChange())
-print(valueChange())
-print(valueChange())
 
-var valueChangeDiff = makeIncrementer(forValue: 7)
-print(valueChangeDiff())
-print(valueChangeDiff())
-print(valueChange())
+let increment = makeIncrement(Increment: 10)
+increment()//calling closure nested function
+increment()
 
-//Closure are reference types
-//when ever constant is assigned to a function or closure, it contains to reference to function or closure
-//but not wrt to changes in function or closure
-let anotherValueChange = valueChange
-print(anotherValueChange())
+let incrementByThirty = makeIncrement(Increment: 30)
+incrementByThirty()
+increment()
+increment()
+incrementByThirty()
 
-//If making closure as @escaping means have to refer self explicitly
-//If making closure as @nonescaping means have to refer self implicitly
-var completionHandlers:[() -> Void] = []
-func someFunctionWithEscapingClosure( completionHandler:@escaping ()->Void) {
-    completionHandlers.append(completionHandler)
+//Function and closure are reference types
+//Though functions are assigned to a constant ; constant will refer to those function or closlure
+//sample resultUpdate above changes, even if the function is assigned to constant
+let constant = increment //Though after assigning it still increments
+constant()
+
+//Escaping
+//@noescaping is by default in swift3 but before it was specified explicitly and @escaping is default behaviour
+//This is for memory optimization (@escaping can store the closure somewhere so memory storage optimization purpose it is not default in swift 3)
+//Closure is said to escaping when closure is called after the function return
+//Storing the closure outside variable and used later with escaping
+var completionHandler:[()->Void] = []
+func functionWithEscaping(closure :@escaping ()->Void) {
+    completionHandler.append(closure)
 }
 
-func someFunctionWithNonescapingClosure(closure:()->Void) {
+//@escaping - refer self explicitly
+//@noescaping - refer self implicitly
+func functionWithNoEscaping(closure:()->Void) {
     closure()
 }
 
-class SomeClass {
-    var x = 10
-    func doSomething() {
-        someFunctionWithEscapingClosure { self.x = 100 }// self explicitly escaping
-        someFunctionWithNonescapingClosure { x = 200 }//self implicitly non escaping // called first
+class TestingEscapingAndNonEscaping {
+    var x:Int = 10
+    func doSomeThing() {
+        functionWithEscaping { //Stored for later use
+            self.x = 30 //Self specified explicitly
+        }
+        functionWithNoEscaping {
+            x = 50
+        }
     }
 }
 
-let instance = SomeClass()
-instance.doSomething()
-print(instance.x)
-completionHandlers.first?()
-print(instance.x)
+var obj = TestingEscapingAndNonEscaping()
+obj.doSomeThing()
+print(obj.x)
+
+completionHandler.first?()
+print(obj.x)
 
 //Autoclosures
-//it doesnt take any argument and when its called it returns the value of expression thats wrapped inside it
-var arrayExamp = ["a","e","i","o","u"]
-print(arrayExamp.count)
-let remove = { arrayExamp.remove(at: 0) }
-print(arrayExamp.count)
-remove()
-print(arrayExamp.count)
+//It doesnt take any argument, when its called, it returns the value of expression thats wrapped inside it
+//omits the braces
+//Autoclosure lets you delay evaluation ; code is not evaluated until the closure is called
+var customerInLine = ["A", "B", "C", "D", "E", "F", "G", "H"]
+print(customerInLine.count)
 
-//Closure as argument to a fuction
-func serve(customer customerProvider:() -> String) {
-    print("Now Serving \(customerProvider())!")
+var customerAutoClosure = {customerInLine.remove(at: 0)}
+print(customerInLine.count)
+customerAutoClosure()
+print(customerInLine.count)
+
+func serve(customer closure:()->String)
+{
+    print("Now Serving \(customerAutoClosure())")
 }
-serve(customer: {arrayExamp.remove(at: 0)})
 
-func serveAutoClose(customer customerProvider:@autoclosure () -> String) {
-    print("Now Serving \(customerProvider())!")
+serve (customer:{customerInLine.remove(at: 0)})
+
+//Autoclosure
+func serve(customer customerProvider: @autoclosure ()->String) {
+        print("Now serving \(customerProvider())!")
 }
-serveAutoClose(customer: arrayExamp.remove(at: 0))//no flower braces
+serve (customer: customerInLine.remove(at: 0))
 
-//both @escape and @autorelease
+//@autoclosure @escaping
 var customerProviders: [() -> String] = []
-
 func collectCustomerProviders(_ customerProvider: @autoclosure @escaping () -> String) {
     customerProviders.append(customerProvider)
 }
-
-collectCustomerProviders(arrayExamp.remove(at: 0))
-collectCustomerProviders(arrayExamp.remove(at: 0))
+collectCustomerProviders(customerInLine.remove(at: 0))
+collectCustomerProviders(customerInLine.remove(at: 0))
 
 print("Collected \(customerProviders.count) closures.")
 
@@ -154,17 +186,12 @@ for customerProvider in customerProviders {
 }
 
 
-
 //Question : Add +5 to a given array and return only even numbers
 let arraySample = [10,5,6,3,4,1,2]
 let addingConstant = arraySample.map({(number)-> Int in return number+5})
 print("Array after adding 5 : \(addingConstant)")
 let fetchOnlyEvenNumbers = addingConstant.filter{$0 % 2 == 0}
 print("After adding only Even Numbers : \(fetchOnlyEvenNumbers)")
-
-
-
-
 
 
 
